6241b9b392bbc263f21aae102929528ac3a1bf9b
saliha youbi <salihayoubi23@outlook.com>
2023-09-27 08:59:55 +0200

    finalisation projet

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,13 +2,20 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {prettyDOM, screen, waitFor, fireEvent} from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
 import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
 
-import router from "../app/Router.js";
+import mockStore from "../__mocks__/store"
+
+import { ROUTES_PATH, ROUTES } from "../constants/routes"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+
+import { bills } from "../fixtures/bills"
+import router from "../app/Router"
+import Bills from "../containers/Bills.js"
+
+jest.mock("../app/store", () => mockStore) // ?
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -32,9 +39,66 @@ describe("Given I am connected as an employee", () => {
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      //Cette fonction de tri soustrait l'élément b de l'élément a. Si le résultat de cette soustraction est positif, cela signifie que b est plus grand que a, et donc, a devrait être placé avant b dans le tableau trié. Cela a pour effet de trier le tableau dans un ordre décroissant.
+      const antiChrono = (a, b) => (b - a)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
   })
+
 })
+
+describe("When I click on the eye icon", () => {
+  test("Then modal with supporting documents appears", async () => {
+    $.fn.modal = jest.fn() // Suppression de l'erreur jQuery
+
+    // Fonction de navigation fictive
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname })
+    }
+
+    // Création d'un affichage factice des notes de frais
+    document.body.innerHTML = BillsUI({ data: bills })
+
+    // Initialisation de la classe Bills
+    const bill = new Bills({ document, onNavigate, localStorage: window.localStorage });
+
+    // Fonction de gestion du clic sur l'icône "eye"
+    const handleClickIconEye = jest.fn((icon) => bill.handleClickIconEye(icon));
+    const iconEye = screen.getAllByTestId("icon-eye")[0];
+    iconEye.addEventListener("click", handleClickIconEye(iconEye));
+    fireEvent.click(iconEye);
+
+    // Assertions
+    expect(handleClickIconEye).toHaveBeenCalled()
+    expect(screen.getByText("Justificatif")).toBeTruthy();
+    expect(screen.getByAltText("Bill")).toBeTruthy();
+  })
+})
+
+describe("When I click on \"Nouvelle note de frais\"", () => {
+  test("Then the invoice creation form appears", async () => {
+    // Fonction de navigation fictive
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname })
+    }
+
+    // Création d'un affichage factice des notes de frais triées par date décroissante
+    document.body.innerHTML = BillsUI({ data: bills.sort((a, b) => new Date(b.date) - new Date(a.date)) })
+
+    // Initialisation de la classe Bills
+    const bill = new Bills({ document, onNavigate, localStorage: window.localStorage });
+    const handleClickNewBill = jest.fn(() => bill.handleClickNewBill());
+
+    // Gestion du clic sur le bouton "Nouvelle note de frais"
+    const btnNewBill = screen.getByTestId("btn-new-bill");
+    btnNewBill.addEventListener("click", handleClickNewBill);
+    fireEvent.click(btnNewBill);
+
+    // Assertions
+    expect(handleClickNewBill).toHaveBeenCalled();
+    expect(screen.getByText("Envoyer une note de frais")).toBeTruthy();
+    expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+  })
+})
+
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,213 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { fireEvent, prettyDOM, screen, waitFor } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import BillsUI from "../views/BillsUI.js";
+import NewBill from "../containers/NewBill.js";
+import mockStore from "../__mocks__/store";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import userEvent from "@testing-library/user-event";
+import router from "../app/Router.js";
+import Store from "../app/Store";
 
+jest.mock('../app/store', () => mockStore)
+// Importation des dépendances et configuration initiale des tests
+describe('Given I am connected as an employee', () => {
+  // Début des tests pour la page NewBill
+  describe('When I am on the NewBill Page and click on the "change file" button', () => {
+      beforeEach(() => {
+          // Configuration initiale des tests : définir localStorage, location, et le contenu HTML du document
+          Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+          Object.defineProperty(window, 'location', { value: { hash: ROUTES_PATH['NewBill'] } });
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+          // Configuration du type d'utilisateur en tant qu'employé et initialisation du contenu HTML
+          window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }));
+          document.body.innerHTML = `<div id="root"></div>`;
+          // Configuration de la navigation
+          router();
+      });
+
+      // Test : Choix d'un fichier avec une extension valide
+      test('Then I can choose a file with a valid extension', async () => {
+          // Création d'une instance de NewBill et configuration d'un gestionnaire d'événements pour le changement de fichier
+          const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage });
+          const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+          const inputFile = screen.getByTestId("file");
+
+          // Création d'un fichier simulé avec une extension valide
+          const file = new File(["contenu du fichier fake"], "facture.png", { type: "image/png" });
+
+          // Ajout du gestionnaire d'événements pour le changement de fichier et simulation du changement de fichier
+          inputFile.addEventListener("change", handleChangeFile);
+          fireEvent.change(inputFile, { target: { files: [file] } });
+
+          // Vérification que le gestionnaire d'événements a été appelé et que le fichier a été correctement sélectionné
+          expect(handleChangeFile).toBeCalled();
+          expect(inputFile.files.length).toBe(1);
+          expect(inputFile.files[0].name).toBe("facture.png");
+      });
+
+      // Test : Choix d'un fichier avec une extension non valide
+      test('Then I can choose a file with an invalid extension', async () => {
+          // Création d'une instance de NewBill et configuration d'un gestionnaire d'événements pour le changement de fichier
+          const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage });
+          const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+          const inputFile = screen.getByTestId("file");
+
+          // Création d'un fichier simulé avec une extension non valide (PDF au lieu de PNG)
+          const file = new File(["contenu du fichier fake"], "facture.pdf", { type: "application/pdf" });
+
+          // Ajout du gestionnaire d'événements pour le changement de fichier et simulation du changement de fichier
+          inputFile.addEventListener("change", handleChangeFile);
+          fireEvent.change(inputFile, { target: { files: [file] } });
+
+          // Vérification que le gestionnaire d'événements a été appelé et que le champ de fichier est vide
+          expect(handleChangeFile).toBeCalled();
+          expect(inputFile.value).toBeFalsy();
+      });
+  });
+
+  // Suite de tests : Remplissage du formulaire avec des données valides et soumission
+  describe('When I fill in the fields with the correct format and click the submit button', () => {
+      test('Then I should post a new Bill ticket', async () => {
+          // Configuration initiale des tests
+          const onNavigate = (pathname) => {
+              document.body.innerHTML = ROUTES({ pathname });
+          };
+
+          // Création d'une instance de NewBill et configuration des valeurs d'entrée
+          const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage });
+          const inputData = {
+              type: 'Transports',
+              name:  'Billet de train Paris - Marseille',
+              amount: '250',
+              date:  '2023-04-25',
+              vat: 80,
+              pct: 25,
+              file: new File(['billet de train'], 'billet.png', { type:'image/png' }),
+              commentary: 'Note de deplacement professionnel',
+              status: 'pending'
+          };
+
+          // Récupération des éléments du formulaire
+          const inputType = screen.getByTestId('expense-type');
+          const inputName = screen.getByTestId('expense-name');
+          const inputDate = screen.getByTestId('datepicker');
+          const inputAmount = screen.getByTestId('amount');
+          const inputVat = screen.getByTestId('vat');
+          const inputPct = screen.getByTestId('pct');
+          const inputComment = screen.getByTestId('commentary');
+          const inputFile = screen.getByTestId('file');
+          const form = screen.getByTestId('form-new-bill');
+
+          // Remplissage des champs de formulaire avec des données valides
+          fireEvent.change(inputType, { target: { value: inputData.type } });
+          fireEvent.change(inputName, { target: { value: inputData.name } });
+          fireEvent.change(inputDate, { target: { value: inputData.date } });
+          fireEvent.change(inputAmount, { target: { value: inputData.amount } });
+          fireEvent.change(inputVat, { target: { value: inputData.vat } });
+          fireEvent.change(inputPct, { target: { value: inputData.pct } });
+          fireEvent.change(inputComment, { target: { value: inputData.commentary } });
+
+          // Configuration des gestionnaires d'événements pour le changement de fichier et la soumission du formulaire
+          const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+          const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+          inputFile.addEventListener("change", handleChangeFile);
+          form.addEventListener('submit', handleSubmit);
+
+          // Simulation de l'envoi du fichier et de la soumission du formulaire
+          userEvent.upload(inputFile, inputData.file);
+          fireEvent.submit(form);
+
+          // Vérification que le gestionnaire de soumission a été appelé et que les champs de formulaire sont valides
+          expect(handleSubmit).toHaveBeenCalled();
+          expect(inputType.validity.valid).toBeTruthy();
+          expect(inputName.validity.valid).toBeTruthy();
+          expect(inputDate.validity.valid).toBeTruthy();
+          expect(inputAmount.validity.valid).toBeTruthy();
+          expect(inputVat.validity.valid).toBeTruthy();
+          expect(inputPct.validity.valid).toBeTruthy();
+          expect(inputComment.validity.valid).toBeTruthy();
+          expect(inputFile.files[0]).toBeDefined();
+      });
+
+      // Test : Rendu de la page des factures après la soumission réussie du formulaire
+      test('Then it should render the Bills Page', () => {
+          // Vérification que la page des factures est correctement rendue après la soumission du formulaire
+          expect(screen.getAllByText('Mes notes de frais')).toBeTruthy();
+      });
+  });
+
+  // Suite de tests : Remplissage du formulaire avec des données incorrectes et soumission
+  describe('When I fill in the fields with incorrect format and click the submit button', () => {
+      test('Then I should have an HTML validation error in the form', async () => {
+          // Configuration initiale des tests
+          Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+          Object.defineProperty(window, 'location', { value: { hash: ROUTES_PATH['NewBill'] } });
+
+          // Configuration du type d'utilisateur en tant qu'employé et initialisation du contenu HTML
+          window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }));
+          document.body.innerHTML = `<div id="root"></div>`;
+          // Configuration de la navigation
+          router();
+
+          // Configuration du gestionnaire de navigation
+          const onNavigate = (pathname) => {
+              document.body.innerHTML = ROUTES({ pathname });
+          };
+
+          // Création d'une instance de NewBill et configuration des valeurs d'entrée avec des données incorrectes
+          const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage });
+          const inputData = {
+              type: 'test',
+              name:  'Vol Paris - Berlin',
+              amount: 'test',
+              date:  'date incorrecte',
+              vat: 70,
+              pct: 'test',
+              file: new File(['img'], 'image.png', { type: 'image/png' }),
+              commentary: 'Note de deplacement professionnel',
+              status: 'pending'
+          };
+
+          // Récupération des éléments du formulaire
+          const inputType = screen.getByTestId('expense-type');
+          const inputName = screen.getByTestId('expense-name');
+          const inputDate = screen.getByTestId('datepicker');
+          const inputAmount = screen.getByTestId('amount');
+          const inputVat = screen.getByTestId('vat');
+          const inputPct = screen.getByTestId('pct');
+          const inputComment = screen.getByTestId('commentary');
+          const inputFile = screen.getByTestId('file');
+          const form = screen.getByTestId('form-new-bill');
+
+          // Remplissage des champs de formulaire avec des données incorrectes
+          fireEvent.change(inputType, { target: { value: inputData.type } });
+          fireEvent.change(inputName, { target: { value: inputData.name } });
+          fireEvent.change(inputDate, { target: { value: inputData.date } });
+          fireEvent.change(inputAmount, { target: { value: inputData.amount } });
+          fireEvent.change(inputVat, { target: { value: inputData.vat } });
+          fireEvent.change(inputPct, { target: { value: inputData.pct } });
+          fireEvent.change(inputComment, { target: { value: inputData.commentary } });
+
+          // Simulation de l'envoi du fichier
+          userEvent.upload(inputFile, inputData.file);
+
+          // Configuration du gestionnaire d'événements pour la soumission du formulaire
+          const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+          form.addEventListener('submit', handleSubmit);
+
+          // Simulation de la soumission du formulaire
+          fireEvent.submit(form);
+
+          // Vérification que le gestionnaire de soumission a été appelé et que les champs de formulaire sont invalides
+          expect(handleSubmit).toHaveBeenCalled();
+          expect(inputType.validity.valid).not.toBeTruthy();
+          expect(inputDate.validity.valid).not.toBeTruthy();
+          expect(inputAmount.validity.valid).not.toBeTruthy();
+          expect(inputPct.validity.valid).not.toBeTruthy();
+      });
+  });
+});
